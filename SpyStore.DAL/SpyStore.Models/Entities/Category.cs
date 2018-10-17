82f51a5fb6b63c76e87bafc8bf027c07051f571e
Krish Kumar <Krish.Kumar@asggroup.com.au>
2018-10-17 12:29:54 +1000

    Db Set and Abstration

---
+++ SpyStore.DAL/SpyStore.Models/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using SpyStore.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpyStore.Models.Entities
+{
+    [Table ("Category",Schema ="Store")]
+    public class Category : EntityBase
+    {
+        [DataType(DataType.Text),MaxLength(50)]
+        public string CategoryName { get; set; }
+    }
+}
